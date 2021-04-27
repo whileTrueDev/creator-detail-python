4b2ef6ceae0bcca2ee43f42dd9b6a68a493e2540
chanuuuuu <dn0208@gmail.com>
2021-04-27 10:23:14 +0900

    data 수정 적용

--- run.py
+++
@@ -1,9 +0,0 @@
-from collectors.collector import Collector
-
-if __name__ == '__main__':
-    collector = Collector()
-    collector.process()
-
-    # afreeca platform 사용
-    collector.set_platform('afreeca')
-    collector.process()
